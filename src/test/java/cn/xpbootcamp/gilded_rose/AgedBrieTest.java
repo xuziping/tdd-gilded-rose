7eebd633b482928d48e09f68e574511b190b8e41
XuZhiPing <zhiping.xu@thoughtworks.com>
2020-03-22 16:49:55 +0800

    add new test case - AgedBrie quality is always up

---
+++ src/test/java/cn/xpbootcamp/gilded_rose/AgedBrieTest.java
@@ -0,0 +1,22 @@
+package cn.xpbootcamp.gilded_rose;
+
+import cn.xpbootcamp.gilded_rose.domain.entity.AgedBrieGoods;
+import cn.xpbootcamp.gilded_rose.domain.entity.Goods;
+import com.spun.util.Asserts;
+import org.junit.jupiter.api.Test;
+
+public class AgedBrieTest {
+
+    private static final Integer OVER_SELLIN_DAYS = 50;
+
+    @Test
+    public void quality_is_higher_by_days(){
+        Goods goods = new AgedBrieGoods();
+        int previousQuality = goods.getOriginalQuality();
+        for (int days = 0; days < goods.getSellIn() + OVER_SELLIN_DAYS; days++) {
+            int currentQuality = goods.calQuality(days);
+            Asserts.assertTrue(String.format("%s after %d ", goods.getName(), days), currentQuality >= previousQuality);
+            previousQuality = currentQuality;
+        }
+    }
+}
