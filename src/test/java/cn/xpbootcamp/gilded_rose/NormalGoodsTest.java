2061f16358b244699296f9208190360f730a8d12
XuZhiPing <zhiping.xu@thoughtworks.com>
2020-03-22 18:06:04 +0800

    add test case - Normal goods rules

--- src/test/java/cn/xpbootcamp/gilded_rose/NormalGoodsTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/NormalGoodsTest.java
@@ -1,23 +1,25 @@
 package cn.xpbootcamp.gilded_rose;
 
 import cn.xpbootcamp.gilded_rose.domain.entity.Goods;
-import cn.xpbootcamp.gilded_rose.domain.entity.SulfurasGoods;
+import cn.xpbootcamp.gilded_rose.domain.entity.NormalGoods;
 import com.spun.util.Asserts;
 import org.junit.jupiter.api.Test;
 
-public class SulfurasTest {
+public class NormalGoodsTest {
 
     private static final Integer OVER_SELLIN_DAYS = 50;
 
     @Test
-    public void quality_should_no_change() {
-        Goods goods = new SulfurasGoods();
-        int dayIndex = 1;
+    public void normal_goods_quality_double_down_after_sellin() {
+
+        Goods goods = new NormalGoods();
+        int dayIndex = goods.getSellIn();
         int previousQuality = goods.calQuality(dayIndex);
+        int downQuality = goods.calQuality(dayIndex - 1) - previousQuality;
         for (dayIndex++; dayIndex < goods.getSellIn() + OVER_SELLIN_DAYS; dayIndex++) {
             int currentQuality = goods.calQuality(dayIndex);
             Asserts.assertTrue(String.format("%s after %d ", goods.getName(), dayIndex),
-                currentQuality == previousQuality);
+                (previousQuality-currentQuality) == downQuality * 2);
             previousQuality = currentQuality;
         }
     }
